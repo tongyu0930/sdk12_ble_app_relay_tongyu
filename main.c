133b3465fa089f8f2127913d5e5f0b2beca8be3c
Tong Yu <yutong0815@gmail.com>
2017-01-01 15:32:32 +0100

    worked
    一开始advertise，按下按键1（按住）后停止广播，然后开是扫描，扫描到字母a后led3闪烁。

--- main.c
+++ main.c
@@ -30,6 +30,12 @@
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
+#include "nrf_drv_gpiote.h"
+#include "boards.h"
+#include "app_error.h"
+
+
+
 #define CENTRAL_LINK_COUNT              0                                 /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
 #define PERIPHERAL_LINK_COUNT           0                                 /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
@@ -42,10 +48,30 @@
 
 #define DEAD_BEEF                       0xDEADBEEF                        /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
-#define APP_TIMER_PRESCALER             0                                 /**< Value of the RTC1 PRESCALER register. */
-#define APP_TIMER_OP_QUEUE_SIZE         4                                 /**< Size of timer operation queues. */
+//不知道设置为0004和4000有什么区别，led看起来都闪的一样快。
+//#define SCAN_INTERVAL           0x00A0                          /**< Determines scan interval in units of 0.625 millisecond. */
+#define SCAN_INTERVAL           0x0400   //Scan interval or window is between 0x0004 and 0x4000 in 0.625ms units (2.5ms to 10.24s).
+//#define SCAN_WINDOW             0x0050                          /**< Determines scan window in units of 0.625 millisecond. */
+#define SCAN_WINDOW             0x0400   //The scanWindow shall be less than or equal to the scanInterval.Scan window between 0x0004 and 0x4000
+#define SCAN_ACTIVE             0                               /**< If 1, performe active scanning (scan requests). */
+#define SCAN_SELECTIVE          0                               /**< If 1, ignore unknown devices (non whitelisted). */
+#define SCAN_TIMEOUT            0x0000                          /**< Timout when scanning. 0x0000 disables timeout. */
 
 static ble_gap_adv_params_t m_adv_params;                                 /**< Parameters to be passed to the stack when starting advertising. */
+
+
+/**
+ * @brief Parameters used when scanning.   active scan means asking for scan response packet
+ */
+static const ble_gap_scan_params_t m_scan_params =
+  {
+    .active      = SCAN_ACTIVE,
+    .use_whitelist   = SCAN_SELECTIVE,
+    .adv_dir_report = 0,
+    .interval    = SCAN_INTERVAL,
+    .window      = SCAN_WINDOW,
+    .timeout     = SCAN_TIMEOUT
+  };
 
 
 /**@brief Callback function for asserts in the SoftDevice.
@@ -118,6 +144,77 @@ static void advertising_start(void)
 }
 
 
+static bool is_uuid_present(const ble_gap_evt_adv_report_t *p_adv_report)
+{
+    uint32_t index = 0; //关于report详解，参见“BLE central tutorial”中scanning部分内容
+    uint8_t *p_data = (uint8_t *)p_adv_report->data;
+
+    while (index < p_adv_report->dlen)
+    {
+        uint8_t field_length = p_data[index];
+        uint8_t field_type   = p_data[index+1];
+
+			if ( field_type == BLE_GAP_AD_TYPE_MANUFACTURER_SPECIFIC_DATA)
+			{
+
+				uint8_t a=index+2;
+
+				while(a <= (index+field_length))
+				{
+					uint8_t field_data = p_data[a];
+					if(field_data == 0x61)
+					{
+						return true;
+					}
+					a++;
+				}
+			}
+
+            index += field_length + 1;
+    }
+    return false;
+}
+
+
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @param[in] p_ble_evt  Bluetooth stack event.
+ */
+static void on_ble_evt(ble_evt_t * p_ble_evt)
+{
+    const ble_gap_evt_t * p_gap_evt = &p_ble_evt->evt.gap_evt;
+
+    switch (p_ble_evt->header.evt_id)
+    {
+        case BLE_GAP_EVT_ADV_REPORT:
+        {
+            const ble_gap_evt_adv_report_t * p_adv_report = &p_gap_evt->params.adv_report;
+
+            if (is_uuid_present(p_adv_report))
+            {
+                nrf_drv_gpiote_out_toggle(BSP_LED_3); // 自己加的 因为一只在scan mode上，所以闪一下就又换到scan闪烁状态了。
+            }
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+/**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
+ *
+ * @details This function is called from the scheduler in the main loop after a BLE stack event has
+ *          been received.
+ *
+ * @param[in] p_ble_evt  Bluetooth stack event.
+ */
+static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
+{
+    on_ble_evt(p_ble_evt);
+}
+
+
 /**@brief Function for initializing the BLE stack.
  *
  * @details Initializes the SoftDevice and the BLE event interrupt.
@@ -143,6 +240,48 @@ static void ble_stack_init(void)
     // Enable BLE stack.
     err_code = softdevice_enable(&ble_enable_params);
     APP_ERROR_CHECK(err_code);
+
+    // Register with the SoftDevice handler module for BLE events.
+    err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) //这个东西就是当设置的input被激发时，所唤起的function
+                                                                            //这里的“action”有三种actions，是哪一种啊？
+{
+    sd_ble_gap_adv_stop(); // 去掉这句话，按下按键，系统就会自动不断的切换广播和扫描。
+    uint32_t err_code;
+    err_code = sd_ble_gap_scan_start(&m_scan_params);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+static void gpio_config()
+{
+	ret_code_t err_code;
+
+    // Initialze driver.
+    err_code = nrf_drv_gpiote_init(); //Function for initializing the GPIOTE module.
+    APP_ERROR_CHECK(err_code);
+
+    // Configure output LED3
+    nrf_drv_gpiote_out_config_t out_config = GPIOTE_CONFIG_OUT_SIMPLE(false);
+    err_code = nrf_drv_gpiote_out_init(BSP_LED_3, &out_config);
+    APP_ERROR_CHECK(err_code);
+    // Set output pins (this will turn off the LED's). 这个function的意思是设置为低电平？
+    nrf_drv_gpiote_out_set(BSP_LED_3);
+
+    // Configure input Button1
+    nrf_drv_gpiote_in_config_t in_config = GPIOTE_CONFIG_IN_SENSE_TOGGLE(true); //对于in_config这个结构体里，high_accuracy=ture, is_watcher＝false，
+                                                                                    //提升电阻默认关，sense = NRF_GPIOTE_POLARITY_TOGGLE（sense对应上面的action，这里设置input电位有变化时激发interrupt）
+    in_config.pull = NRF_GPIO_PIN_PULLUP; //对于上面in_config这个结构体的其中一个元素的设置。Pin pullup resistor enabled。因为GPIOTE_CONFIG_IN_SENSE_TOGGLE()里面提升电阻默认是关闭的。
+
+    err_code = nrf_drv_gpiote_in_init(BUTTON_1, &in_config, in_pin_handler); // 这个input被触发时就激发in_pin_handler, 其实就是说input被按下时就呼叫in_pin_handler
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_gpiote_in_event_enable(BUTTON_1, true); //Function for enabling sensing of a GPIOTE input pin.
+    //true to enable the interrupt.
 }
 
 
@@ -165,10 +304,11 @@ int main(void)
     err_code = NRF_LOG_INIT(NULL);
     APP_ERROR_CHECK(err_code);
 
-    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
-    err_code = bsp_init(BSP_INIT_LED, APP_TIMER_TICKS(100, APP_TIMER_PRESCALER), NULL);
-    APP_ERROR_CHECK(err_code);
+    //APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+    //err_code = bsp_init(BSP_INIT_LED, APP_TIMER_TICKS(100, APP_TIMER_PRESCALER), NULL);
+    //APP_ERROR_CHECK(err_code);
     ble_stack_init();
+    gpio_config();
     advertising_init();
 
     // Start execution.
@@ -178,6 +318,8 @@ int main(void)
     // Enter main loop.
     for (;; )
     {
+
+
         if (NRF_LOG_PROCESS() == false)
         {
             power_manage();
